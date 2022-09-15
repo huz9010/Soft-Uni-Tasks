b8cb07df40a9bb82deae6d0374e346f7863bfd51
Hyusein Nazar <makaveli9010@gmail.com>
2022-09-15 15:19:52 +0300

    AnonymousThreat updated (not finished)

--- Programming Fundamentals/Lists/src/ListsExercise/AnonymousThreat.java
+++ Programming Fundamentals/Lists/src/ListsExercise/AnonymousThreat.java
@@ -1,5 +1,6 @@
 package ListsExercise;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -12,16 +13,20 @@ public class AnonymousThreat {
         List<String> inputList = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
         String input = scanner.nextLine();
 
-        while (!input.equals("3:1"))    {
-            String [] command = input.split(" ");
+        while (!input.equals("3:1")) {
+            String[] command = input.split(" ");
             switch (command[0]) {
                 case "merge":
                     int startIndex = Integer.parseInt(command[1]);
                     int endIndex = Integer.parseInt(command[2]);
+                    if (startIndex >= inputList.size()) {
+                        input = scanner.nextLine();
+                        continue;
+                    }
                     if (startIndex < 0) {
                         startIndex = 0;
                     }
-                    if (endIndex >= inputList.size())   {
+                    if (endIndex >= inputList.size()) {
                         endIndex = inputList.size() - 1;
                     }
                     String tempString = "";
@@ -29,17 +34,28 @@ public class AnonymousThreat {
                         tempString += inputList.get(i);
                     }
                     inputList.removeAll(inputList.subList(startIndex, endIndex + 1));
-                    inputList.add(0, tempString);
+                    inputList.add(startIndex, tempString);
                     break;
                 case "divide":
                     int index = Integer.parseInt(command[1]);
                     int partitions = Integer.parseInt(command[2]);
                     String currentElement = inputList.get(index);
-                    if (currentElement.length() % partitions == 0)  {
-
+                    int charsInPart = currentElement.length() / partitions;
+                    List<String> equalStr = new ArrayList<>();
+                    if (currentElement.length() % partitions == 0) {
+                        for (int i = 0; i < currentElement.length(); i += charsInPart) {
+                            String part = currentElement.substring(i, i + charsInPart);
+                            equalStr.add(part);
+                        }
                     }
+                    inputList.remove(index);
+                    inputList.addAll(index - 1, equalStr);
                     break;
             }
+            input = scanner.nextLine();
+        }
+        for (String element : inputList)    {
+            System.out.print(element + " ");
         }
     }
 }
