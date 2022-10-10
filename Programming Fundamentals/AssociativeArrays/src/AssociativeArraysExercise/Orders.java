88f2a282a1361cf398f16327c5cede0db5f9b36b
Hyusein Nazar <makaveli9010@gmail.com>
2022-10-10 12:18:01 +0300

    Orders task from Associative Arrays lab added(working)

---
+++ Programming Fundamentals/AssociativeArrays/src/AssociativeArraysExercise/Orders.java
@@ -0,0 +1,37 @@
+package AssociativeArraysExercise;
+
+import java.util.*;
+
+public class Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        LinkedHashMap<String, List<Double>> productMap = new LinkedHashMap<>();
+
+        while (!input.equals("buy"))    {
+            List<Double> priceAndQuantity = new ArrayList<>();
+            String[] products = input.split(" ");
+            String product = products[0];
+            double price = Double.parseDouble(products[1]);
+            double quantity = Double.parseDouble(products[2]);
+
+            if (!productMap.containsKey(product))   {
+                priceAndQuantity.add(price);
+                priceAndQuantity.add(quantity);
+                productMap.put(product, priceAndQuantity);
+            }   else {
+                priceAndQuantity.add(0, price);
+                double currentQuantity = productMap.get(product).get(1) + quantity;
+                priceAndQuantity.add(1, currentQuantity);
+                productMap.put(product, priceAndQuantity);
+            }
+            input = scanner.nextLine();
+        }
+        for (Map.Entry<String, List<Double>> entry : productMap.entrySet()) {
+            double totalPrice = entry.getValue().get(0) * entry.getValue().get(1);
+            System.out.printf("%s -> %.2f%n", entry.getKey(), totalPrice);
+        }
+
+    }
+}
