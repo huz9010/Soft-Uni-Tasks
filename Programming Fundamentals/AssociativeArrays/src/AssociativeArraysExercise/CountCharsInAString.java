852d09f5e240ed2001b7d018fbfefd206b0fff24
Hyusein Nazar <makaveli9010@gmail.com>
2022-10-10 11:47:56 +0300

    CountCharsInAString task from Associative Arrays lab added(working)

---
+++ Programming Fundamentals/AssociativeArrays/src/AssociativeArraysExercise/CountCharsInAString.java
@@ -0,0 +1,27 @@
+package AssociativeArraysExercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        LinkedHashMap<Character, Integer> charCount = new LinkedHashMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.compare(input.charAt(i), ' ') != 0) {
+                if (!charCount.containsKey(input.charAt(i)))    {
+                    charCount.put(input.charAt(i), 1);
+                }   else {
+                    charCount.put(input.charAt(i), charCount.get(input.charAt(i)) + 1);
+                }
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            System.out.printf("%c -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
