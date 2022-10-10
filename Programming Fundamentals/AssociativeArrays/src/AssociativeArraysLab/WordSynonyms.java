de8af644f85ab1a6874e2d7c0c8269a7e5ee5b31
Hyusein Nazar <makaveli9010@gmail.com>
2022-10-10 10:54:10 +0300

    WordSynonyms task from Associative Arrays lab added(working)

---
+++ Programming Fundamentals/AssociativeArrays/src/AssociativeArraysLab/WordSynonyms.java
@@ -0,0 +1,25 @@
+package AssociativeArraysLab;
+
+import java.util.*;
+
+public class WordSynonyms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        LinkedHashMap<String, List<String>> synonymMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+            if (!synonymMap.containsKey(word))  {
+                synonymMap.put(word, new ArrayList<>());
+            }
+            synonymMap.get(word).add(synonym);
+        }
+
+        for (Map.Entry<String, List<String>> entry : synonymMap.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+    }
+}
