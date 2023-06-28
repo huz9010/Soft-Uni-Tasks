166d3a9dd298224c3c83ec94c6353ba9bc10401e
Hyusein Nazar <makaveli9010@gmail.com>
2023-06-28 23:36:24 +0300

    Introduction to Hibernate Completed

--- Java DB/Spring Data/Introduction to Hibernate/src/main/java/FindLatest10Projects.java
+++ Java DB/Spring Data/Introduction to Hibernate/src/main/java/FindLatest10Projects.java
@@ -1,12 +1,14 @@
 import entities.Project;
 
+import java.util.Comparator;
+
 public class FindLatest10Projects {
     public static void main(String[] args) {
 
         Setup.createEntityManager().createQuery("FROM Project ORDER BY startDate DESC", Project.class)
                 .setMaxResults(10)
                 .getResultList()
-                .stream().sorted((p1, p2) -> p1.getName().compareTo(p2.getName()))
+                .stream().sorted(Comparator.comparing(Project::getName))
                 .forEach(Project::printProjectInfo);
     }
 }
